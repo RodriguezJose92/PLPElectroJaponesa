40350b8eb4ad90cdcdf39e5873be5ea063a62b5f
Jose Rodriguez <joserodriguez@mudi.com.co>
2023-12-29 15:01:30 -0500

    modificacion v2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let flags = document.querySelectorAll('.realidad-aumentada');
 let counter = 0
 
 const intervalo = setInterval(()=>{
-    if(counter==10)clearInterval(intervalo)
-    else if (!flags) counter++
+    if(counter==10){clearInterval(intervalo) ; console.log('limpie el intervalo')}
+    else if (!flags) {counter++; console.log('sigo buscando el contenedor')}
     else {
         let parentElement = null;
 
@@ -11,6 +11,8 @@ const intervalo = setInterval(()=>{
             parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p')
             parentElement.appendChild(flags[i])
         }
+
+        console.log('hecho')
 
     }
 },1000)
