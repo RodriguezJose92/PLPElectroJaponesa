590e45bce5b54420e034898f93173109bd7c491b
Jose Rodriguez <joserodriguez@mudi.com.co>
2023-12-29 17:09:04 -0500

    version 6

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const intervalo = setInterval(()=>{
         let parentElement = null;
 
         for( i=0; i<flags.length; i++){
-            parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p')
-            parentElement.appendChild(flags[i])
+            parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p');
+            if(!parentElement) flags[i].remove();
+            else parentElement.appendChild(flags[i])
         }
         clearInterval(intervalo);
     };
