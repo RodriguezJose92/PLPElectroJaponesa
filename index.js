0353779b05d425c36294d9aabc4b8625eb891eee
RodriguezJose92 <joserodriguez@mudi.com.co>
2024-01-03 17:27:52 -0500

    versión 5 arreglo de la lista de proiductos

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
-let flags = document.querySelectorAll('.realidad-aumentada');
-let counter = 0;
-let intervalo = setInterval(()=>{
-    if( counter==10 ){clearInterval(intervalo)}
-    else if ( flags.length==0 ) { counter++; flags = document.querySelectorAll('.realidad-aumentada');}
-    else {
-        let parentElement = null;
+let flags       = null;
+let counter     = 0;
 
-        for( i=0; i<flags.length; i++){
-            parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p');
-            if(!parentElement) flags[i].remove();
-            else parentElement.appendChild(flags[i])
-        }
-        clearInterval(intervalo);
-    };
-},1000);
+// Verificación de que las etiquetas de realidad aumentada existen 
+function verifyTagAR(){
+    counter = 0;
+    flags = document.querySelectorAll('.realidad-aumentada');
+    let intervalo = setInterval(()=>{
+        if( counter==10 ){clearInterval(intervalo)}
+        else if ( flags.length==0 ) { counter++; flags = document.querySelectorAll('.realidad-aumentada');}
+        else {
+            let parentElement = null;
+    
+            for( i=0; i<flags.length; i++){
+                parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p');
+                if(!parentElement) flags[i].remove();
+                else parentElement.appendChild(flags[i])
+            }
+            clearInterval(intervalo);
+        };
+    },500);
+}
 
-let paginationList = document.querySelectorAll('.page-number');
-let counterPagination = 0;
+// Ejecutamos la función una vez
+verifyTagAR()
 
-const intervalPagination = setInterval(()=>{
-    if(counterPagination==10){ 
-        clearInterval(intervalPagination); 
-        console.log('limpiado por llegar a 10')
-    }
-    else if( paginationList.length==0){
-        counterPagination++;
+// Asociamos un evento a la lista de paginación para que la verificacion de las etiquetas AR también suceda aquí en esta sección 
+const paginationList = document.querySelectorAll('.page-number');
+let intervalo1 = setInterval(()=>{
+    if(paginationList.length==0){
         paginationList = document.querySelectorAll('.page-number');
-        console.log('reiterando Lista ')
     }else{
-        counter=0;
-        intervalo = setInterval(()=>{
-            if( counter==10 ){clearInterval(intervalo)}
-            else if ( flags.length==0 ) { counter++; flags = document.querySelectorAll('.realidad-aumentada');}
-            else {
-                let parentElement = null;
-        
-                for( i=0; i<flags.length; i++){
-                    parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p');
-                    if(!parentElement) flags[i].remove();
-                    else parentElement.appendChild(flags[i])
-                }
-                clearInterval(intervalo);
-            };
-        },1000);
-        clearInterval(intervalPagination);
-        console.log('hecho')
-    }
-},1000)
+        for(i = 0 ; i<paginationList.length; i++ ){paginationList[i].addEventListener('click',verifyTagAR)};
+        clearInterval(intervalo1);
+    };
+},500);
+
+
 
