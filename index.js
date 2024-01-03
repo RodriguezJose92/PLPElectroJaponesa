6acb5822e54bffe43c9b166f0bb14601597406c3
RodriguezJose92 <joserodriguez@mudi.com.co>
2024-01-03 16:50:12 -0500

    prueba 2 del Evento de la lista

--- index.js
+++ index.js
@@ -19,16 +19,7 @@ const listNumberItems = document.querySelectorAll('.page-number');
 
 for(let i = 0; i<listNumberItems.length; i++){
     listNumberItems[i].addEventListener('click',()=>{
-        setTimeout(()=>{
-            flags = document.querySelectorAll('.realidad-aumentada');
-            let parentElement = null;
-
-            for( i=0; i<flags.length; i++){
-            parentElement =  flags[i].parentNode.parentNode.querySelector('.vitrin-home .img_p');
-            if(!parentElement) flags[i].remove();
-            else parentElement.appendChild(flags[i])
-        }
-        },1000)
+        console.log('añadido un evento nuevo')
     })
 }
 
